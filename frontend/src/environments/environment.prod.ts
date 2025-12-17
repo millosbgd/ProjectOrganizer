de2b757544b33365c4ccb65a9d54429d4d906354
Milos Novakovic <milos.novakovic@cycle.rs>
2025-12-17 13:54:06 +0100

    Fix backend API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://projectorganizer-api.azurewebsites.net/api',
+  apiUrl: 'https://projectorganizer-bxcdcbbuhxayacbb.westeurope-01.azurewebsites.net/api',
   auth0: {
     domain: 'dev-gp57sox40kt34si8.us.auth0.com',
     clientId: 'rOVVRDGgscn6oAqvO0vu09kA61JUKmPp',
